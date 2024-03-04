128f4bab3702a966ca4b2960af38d69692b707fd
PaulTai24 <155749832+PaulTai24@users.noreply.github.com>
2024-03-04 10:45:46 +0800

    COVID SQL Portfolio Project.sql

--- COVID SQL Portfolio Project.sql
+++ COVID SQL Portfolio Project.sql
@@ -1,3 +1,8 @@
+/*
+Covid 19 Data Exploration 
+Skills used: Joins, CTEs, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+*/
+
 SELECT *
 FROM PortfolioProject..CovidDeaths
 
